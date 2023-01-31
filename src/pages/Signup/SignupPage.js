dcfdff6b2176681a1bbe6988761e9f97191f9567
drdoof <shubhang20474@iiitd.ac.in>
2023-01-31 18:35:16 +0530

    updated sign up page styling

--- src/pages/Signup/SignupPage.js
+++ src/pages/Signup/SignupPage.js
@@ -2,79 +2,83 @@ import React from 'react';
 import {Label, TextInput, Button} from 'flowbite-react'
 
 const SignupPage = () => (
-    <form className="flex flex-col gap-4">
-        <div>
-            <div className="mb-2 block">
-                <Label
-                    htmlFor="name"
-                    value="Name"
-                />
-            </div>
-            <TextInput
-                id="name"
-                type="text"
-            />
+    <div className='flex justify-center items-center w-screen h-screen'>
+        <div className='flex justify-center items-center bg-white w-96 py-8 rounded-lg shadow-lg'>
+            <form className="flex flex-col gap-4">
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="name"
+                            value="Name"
+                        />
+                    </div>
+                    <TextInput
+                        id="name"
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="phoneNumber"
+                            value="Phone number"
+                        />
+                    </div>
+                    <TextInput
+                        id="phoneNumber"
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                    <Label
+                        htmlFor="username"
+                        value="Enter new username"
+                    />
+                    </div>
+                    <TextInput
+                    id="username"
+                    type="email"
+                    placeholder="abc@xyz.com"
+                    required={true}
+                    shadow={true}
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                        htmlFor="password"
+                        value="Enter password"
+                        />
+                    </div>
+                    <TextInput
+                        id="password2"
+                        type="password"
+                        required={true}
+                        shadow={true}
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                        htmlFor="repeat-password"
+                        value="Repeat password"
+                        />
+                    </div>
+                    <TextInput
+                        id="repeat-password"
+                        type="password"
+                        required={true}
+                        shadow={true}
+                    />
+                </div>
+                
+                <Button type="submit">
+                    Register new account
+                </Button>
+            </form>
         </div>
-        <div>
-            <div className="mb-2 block">
-                <Label
-                    htmlFor="phoneNumber"
-                    value="Phone number"
-                />
-            </div>
-            <TextInput
-                id="phoneNumber"
-                type="text"
-            />
-        </div>
-        <div>
-            <div className="mb-2 block">
-            <Label
-                htmlFor="username"
-                value="Enter new username"
-            />
-            </div>
-            <TextInput
-            id="username"
-            type="email"
-            placeholder="abc@xyz.com"
-            required={true}
-            shadow={true}
-            />
-        </div>
-        <div>
-            <div className="mb-2 block">
-                <Label
-                htmlFor="password"
-                value="Enter password"
-                />
-            </div>
-            <TextInput
-                id="password2"
-                type="password"
-                required={true}
-                shadow={true}
-            />
-        </div>
-        <div>
-            <div className="mb-2 block">
-                <Label
-                htmlFor="repeat-password"
-                value="Repeat password"
-                />
-            </div>
-            <TextInput
-                id="repeat-password"
-                type="password"
-                required={true}
-                shadow={true}
-            />
-        </div>
-        
-        <Button type="submit">
-            Register new account
-        </Button>
-    </form>
+    </div>
 );
 
 export default SignupPage;
